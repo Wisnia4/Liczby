9f0c99c89d28dfcdc5679bb3027a8f02bbeb2810
Wisnia4 <szpala27189@student.pwsztar.edu.pl>
2017-05-30 15:56:15 +0200

    add new functions

--- app/app.js
+++ app/app.js
@@ -1,13 +1,49 @@
 
-function areaOfTrapezoid(a, b, h)
-{
-    if (a < 0 || b < 0 || h < 0 || typeof a !== 'number'
-            || typeof b !== 'number' || typeof h !== 'number') {
-        return false;
+function automorficzna(n)
+{       var a,b,
+    m=10;
+
+    b=m;
+    while(n>b)
+    {
+        b*=m;
     }
-    return 0.5 * h * (a + b);
+
+    a = (n*n) % b ;
+
+    if (a==n) return(25);
+    else return("Liczba "+n+" nie jest automorficzna");
+
+}
+
+function rozloz(liczba)
+{
+
+    if (!isNaN(liczba) && liczba > 1){
+        var wynik = "Czynniki pierwsze liczby " + liczba + " to:";
+        var i = 2;
+        var e = Math.floor(Math.sqrt(liczba));
+        while (i <= e) {
+            while ((liczba % i) == 0) {
+                wynik += " " + i;
+                liczba = Math.floor(liczba/i);
+                e = Math.floor(Math.sqrt(liczba));
+            }
+            i++;
+        }
+        if (liczba > 1) wynik += " " + liczba;
+        return(wynik);
+    }
+    else
+    {
+        return('Podano nieprawidłową wartość!');
+
+    }
+    return false;
 }
 
 module.exports = {
-    areaOfTrapezoid: areaOfTrapezoid
+    rozloz :  rozloz,
+    automorficzna: automorficzna
+
 };
